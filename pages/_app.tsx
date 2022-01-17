86dbad1bf73391714accda07a94cd15df22df005
Elena <elenaan@styleseller.co.kr>
2022-01-17 14:50:32 +0900

    ✨ 컴포넌트에 데이터 주입

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { CssBaseline, PaletteMode, ThemeProvider } from '@mui/material';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from '../src/common/createEmotionCache';
 import { globalTheme } from '../src/common/globalTheme';
-import GlobalProvider, { useCounter } from '../src/stores/StoreContext';
+import GlobalProvider from '../src/stores/StoreContext';
 import { observer } from 'mobx-react';
 
 const clientSideEmotionCache = createEmotionCache();
@@ -14,36 +14,17 @@ export type MyAppProps = AppProps & {
 };
 
 const MyApp = observer((props: MyAppProps) => {
-    const [mode, setMode] = useState<PaletteMode>('dark');
+    const [mode, setMode] = useState<PaletteMode>('light');
     const {
         Component,
         pageProps,
         emotionCache = clientSideEmotionCache,
     } = props;
 
-    const counterStore = useCounter();
-
     return (
         <GlobalProvider>
             <CacheProvider value={emotionCache}>
                 <ThemeProvider theme={globalTheme(mode)}>
-                    <button
-                        type='button'
-                        onClick={() =>
-                            setMode((pre) =>
-                                pre === 'light' ? 'dark' : 'light'
-                            )
-                        }
-                    >
-                        토글
-                    </button>
-                    <span>{counterStore.number}</span>
-                    <button onClick={counterStore.increaseNumber}>
-                        increase
-                    </button>
-                    <button onClick={counterStore.decreaseNumber}>
-                        decrease
-                    </button>
                     <CssBaseline />
                     <Component {...pageProps} />
                 </ThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,47 @@
+import React, { useEffect } from 'react';
+import { observer } from 'mobx-react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import ownerService from '../src/service/owner/api';
+import Profile from '../src/model/Profile';
+import { useOwner } from '../src/stores/StoreContext';
+import Link from '../src/model/Link';
 
-const Home: NextPage = () => {
+const Home: NextPage = observer(() => {
+    const fakeId = 1;
+    const ownerStore = useOwner();
+
+    useEffect(() => {
+        const getData = async () => {
+            const profileData = await ownerService.getProfile(fakeId);
+            ownerStore.setProfile(Profile.createFormApi(profileData));
+            const linksData = await ownerService.getLinks(fakeId);
+            ownerStore.setLinks(
+                linksData.map((link: any) => Link.createFormApi(link))
+            );
+        };
+        getData();
+    }, [ownerStore]);
+
     return (
         <>
             <Head>
                 <title>ADMIN TEST</title>
             </Head>
-            <div>프리텐다드 글자 적용 확인</div>
+            <div>
+                <h3>{ownerStore.profile.username}</h3>
+                <h4>{ownerStore.profile.userId}</h4>
+                <p>{ownerStore.profile.email}</p>
+            </div>
+            <div>
+                <ul>
+                    {ownerStore.links.map((link) => (
+                        <li key={link.id}>{link.title}</li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
-};
+});
 
 export default Home;
