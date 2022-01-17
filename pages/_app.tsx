970ed742832855223a8ba72dbcb8e017fd1d8ae6
Elena <elenaan@styleseller.co.kr>
2022-01-17 17:42:34 +0900

    🔧 React-query 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
 import type { AppProps } from 'next/app';
+import { observer } from 'mobx-react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { CssBaseline, PaletteMode, ThemeProvider } from '@mui/material';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from '../src/common/createEmotionCache';
 import { globalTheme } from '../src/common/globalTheme';
 import GlobalProvider from '../src/stores/StoreContext';
-import { observer } from 'mobx-react';
+import Header from '../src/components/Header';
 
 const clientSideEmotionCache = createEmotionCache();
 
 export type MyAppProps = AppProps & {
     emotionCache?: EmotionCache;
 };
+
+const queryClient = new QueryClient();
 
 const MyApp = observer((props: MyAppProps) => {
     const [mode, setMode] = useState<PaletteMode>('light');
@@ -26,7 +31,11 @@ const MyApp = observer((props: MyAppProps) => {
             <CacheProvider value={emotionCache}>
                 <ThemeProvider theme={globalTheme(mode)}>
                     <CssBaseline />
-                    <Component {...pageProps} />
+                    <QueryClientProvider client={queryClient}>
+                        <ReactQueryDevtools initialIsOpen={true} />
+                        <Header />
+                        <Component {...pageProps} />
+                    </QueryClientProvider>
                 </ThemeProvider>
             </CacheProvider>
         </GlobalProvider>
